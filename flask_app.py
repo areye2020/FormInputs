4f47c8d0b110fbd34b02614e949a16e2bffab294
Adriana <areye2020@gmail.com>
2019-01-09 23:30:13 +0000

    I made a list of shows based on genre and each genre had a past a current show. I put that based on the uses input it pickes what show they should watch

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,26 @@ def main():
 
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
+    shows = {"Everybody Hates Chris":0, "Crazy Ex Girlfriend":0,"One Tree Hill":0,"The 100":0,"Smallville":0,"The Flash":0,"Riverdale":0,"The Vampire Diaries":0,"Whose Line is it Anyway?":0,"Americas Next Top Model":0}
+
+    if Genre == "Comedy":
+        shows["Everybody Hates Chris", "Crazy Ex Girlfriend"] += 2
+    elif Genre == "Drama":
+        shows["The 100","One Tree Hill"] += 2
+    elif Genre == "Superhero":
+        shows["The Flash","Smallville"] += 2
+    elif Genre == "Suspence":
+        shows["Riverdale","The Vampire Diaries"] += 2
+    elif Genre == "Reality":
+        shows["Whose Line is it Anyway?","Americas Next Top Model"] += 2
+
+    if Type == "Amortentia":
+        shows["Hufflepuff"] += 2
+    elif Type == "Polyjuice":
+        shows["Slytherin"] += 2
+
+
+    Genre = request.form.get('input_dropdown', '')
     name = request.form.get('input_name', '')
     if name == "":
         return render_template("main_page.html", output="Plese Enter Name")
